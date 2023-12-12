79aa94666f276a198b7e1a8b6e4953527c6fbbb5
morashroom <2717557144@qq.com>
2023-12-12 14:22:06 +0800

    处理数据库

---
+++ java/mapper/PackagesMapper.java
@@ -0,0 +1,9 @@
+package com.dlmu.medicine_take_out.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dlmu.medicine_take_out.entity.Packages;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PackagesMapper extends BaseMapper<Packages> {
+}
---
+++ java/mapper/PackagesMedicineMapper.java
@@ -0,0 +1,10 @@
+package com.dlmu.medicine_take_out.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dlmu.medicine_take_out.entity.PackagesMedicine;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PackagesMedicineMapper extends BaseMapper<PackagesMedicine> {
+
+}
